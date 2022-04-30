c3e327ec316d84a92512813f163b8dea3b39099d
mkcakmak <82233837+mkcakmak@users.noreply.github.com>
2022-04-30 10:14:31 +0200

    Update tokenization-customization-2.2.3.py
    fixed add special case

--- Chapter02/tokenization-customization-2.2.3.py
+++ Chapter02/tokenization-customization-2.2.3.py
@@ -6,6 +6,6 @@ nlp = spacy.load("en_core_web_md")
 doc = nlp("lemme that")
 print([w.text for w in doc])
 
-special_case = [{ORTH: "lem"}, {"ORTH": "me"}]
-nlp.tokenizer.add_special.case("lemme", special_case)
+special_case = [{ORTH: "lem"}, {ORTH: "me"}]
+nlp.tokenizer.add_special_case("lemme", special_case)
 print([w.text for w in nlp("lemme that")])
